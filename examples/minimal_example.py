29b459bb7c30b0d85a7dc40de57e3f3c38603894
Valentin Thorey <v.thorey@gmail.com>
2018-12-17 17:35:59 +0100

    Create minimal_example.py

---
+++ examples/minimal_example.py
@@ -0,0 +1,44 @@
+""" In this minimum example we try to minimize the sinus function between 0 and 2pi"""
+
+from benderclient import Bender
+import numpy as np
+
+# Initialize Bender
+bender = Bender()
+
+# Create an experiment
+bender.create_experiment(
+    name='Minimum example experiment',
+    description='Find minimum of sinus function',
+    metrics=[{"metric_name": "sinus_value", "type": "loss"}],
+)
+
+# Create an algo (here the sinus function with one parameter)
+bender.create_algo(
+    name='Analytic sinus function',
+    hyperparameters=[
+        {
+            "name": 'x',
+            "category": "uniform",
+            "search_space": {
+                "low": 0,
+                "high": 2 * np.pi,
+            },
+        },
+    ]
+)
+
+# Ask bender for values to try
+for _ in range(50):
+    # Get a set of Hyperparameters to test
+    suggestion = bender.suggest(metric="sinus_value", optimizer="parzen_estimator")
+
+    # Run the sinus function
+    sinus_value = np.sin(suggestion["x"])
+
+    # Feed Bender a trial
+    bender.create_trial(
+        hyperparameters=suggestion,
+        results={"sinus_value": sinus_value}
+    )
+    print("x: ", suggestion["x"], " value :", sinus_value)
